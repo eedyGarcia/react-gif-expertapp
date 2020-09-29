88e86e922638b0b09fdbc0b96829e8e7f3cd6a31
Eduardo García <eedy.garcia@gmail.com>
2020-09-28 22:06:12 -0500

    Nueva version

---
+++ docs2/precache-manifest.f560ff8c41a5bc8dda3b17c6213fed77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e9a001c108d30d9be777befce667805",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec3b2725fc805fa997c5",
+    "url": "/static/css/main.b5ec987c.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec3b2725fc805fa997c5",
+    "url": "/static/js/main.031af2fa.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
